09e77af2c49a44105ee24bd31c7b9e2387b68111
jinho6225 <jinho6225@hotmail.com>
2020-02-05 14:07:07 -0800

    02052020

---
+++ Layers in a Rug.js
@@ -0,0 +1,61 @@
+/*
+Layers in a Rug
+Write a function that counts how many concentric layers a rug.
+
+Examples
+countLayers([
+  "AAAA",
+  "ABBA",
+  "AAAA"
+]) ➞ 2
+countLayers([
+  "AAAAAAAAA",
+  "ABBBBBBBA",
+  "ABBAAABBA",
+  "ABBBBBBBA",
+  "AAAAAAAAA"
+]) ➞ 3
+countLayers([
+  "AAAAAAAAAAA",
+  "AABBBBBBBAA",
+  "AABCCCCCBAA",
+  "AABCAAACBAA",
+  "AABCADACBAA",
+  "AABCAAACBAA",
+  "AABCCCCCBAA",
+  "AABBBBBBBAA",
+  "AAAAAAAAAAA"
+]) ➞ 5
+
+Test.assertEquals(countLayers([
+"AAA"]), 1)
+
+Test.assertEquals(countLayers([
+"AAAA",
+"AAAA",
+"AAAA"]), 1)
+
+Notes
+Multiple layers can share the same component so count them separately (example #2).
+Layers will be horizontally and vertically symmetric.
+There will be at least one layer for each rug.
+*/
+
+function countLayers(rug) {
+  debugger;
+  var a = Math.floor(rug.length/2)
+  var b = Math.floor(rug[a].length/2)
+  debugger;
+  var count = 1;
+  for (let i = 0; i < b; i++) {
+    if (rug[a][i] !== rug[a][i+1]) {
+      count++
+    }
+  }
+  return count
+}
+
+countLayers([
+"AAAA",
+"ABBA",
+"AAAA"])
